4f00bfd2f1c273a7fa3d6c60d2e3ce15fc5d2064
Dharshini577 <bharathinagaraju1@gmail.com>
2024-10-15 10:56:58 +0530

    Create Dict1.py

---
+++ Dict1.py
@@ -0,0 +1,7 @@
+'''
+The program takes a dictionary and checks if a given key exists in a dictionary or not. 
+Problem Solution:
+Sample Input :
+Enter key to check : A
+'''
+Key is present and value of the key is: 1
